42df356f605c95fe87f79e99fcb5aecd796a10a0
hi1004 <kipo09@naver.com>
2024-03-27 19:27:41 +0900

    update all ranking

--- src/components/books/ranking/AllRankingList.tsx
+++ src/components/books/ranking/AllRankingList.tsx
@@ -30,8 +30,8 @@ const AllRankingList = ({ books }: { books: RankingBookType[] }) => {
       >
         {books?.map((book) => (
           <SwiperSlide key={book.rank} className=" bg-no-repeat bg-white">
-            <div className="grid grid-cols-1 sm:grid-cols-2">
-              <div className="hidden flex-col xl:flex justify-end mb-10">
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+              <div className="hidden flex-col lg:w-[450px] ml-14 lg:flex justify-end mb-10">
                 <Flex align="center">
                   <Text typography="t7" color="gray600" bold className="mr-4">
                     ビズニスマナー ランキング
@@ -52,20 +52,16 @@ const AllRankingList = ({ books }: { books: RankingBookType[] }) => {
                   詳細を見る
                 </Button>
               </div>
-              <Flex
-                align="center"
-                justify="space-around"
-                className="h-[400px] py-7 px-8 bg-primary/5 flex-col "
-              >
+              <div className="sm:h-[400px] sm:flex-row flex py-7 px-12 gap-10 justify-center sm:justify-normal lg:justify-center items-center bg-primary/5 ">
                 <Image
                   src={book.mediumImageUrls[0].replace('ex=', '128x128x')}
                   width={0}
                   height={0}
                   sizes="100vw"
                   alt={book.itemCaption}
-                  className="w-auto h-[300px] object-cover"
+                  className="w-auto h-[128px] sm:h-[300px] object-cover"
                 />
-                <div className="flex-col xl:hidden justify-end">
+                <div className="flex-col lg:hidden justify-end sm:h-[300px] sm:w-full relative">
                   <Flex align="center">
                     <Text typography="t7" color="gray600" bold className="mr-4">
                       総合ランキング
@@ -74,20 +70,27 @@ const AllRankingList = ({ books }: { books: RankingBookType[] }) => {
                       {book.rank}位
                     </Text>
                   </Flex>
-                  <Text typography="t4" as="h1" bold className="text-balance">
+                  <Text
+                    typography="t4"
+                    as="h1"
+                    bold
+                    className="text-balance sm:block hidden"
+                  >
                     {truncateText(book.itemName, 40)}
                   </Text>
 
-                  <div className="mt-1 my-6">{book.reviewAverage}</div>
+                  <div className="sm:mt-1 sm:mb-6 mb-3">
+                    {book.reviewAverage}
+                  </div>
                   <Button
-                    size="medium"
+                    size="small"
                     color="error"
-                    className="sm:max-w-[120px]"
+                    className="sm:max-w-[120px] sm:absolute bottom-0 right-0"
                   >
                     詳細を見る
                   </Button>
                 </div>
-              </Flex>
+              </div>
             </div>
           </SwiperSlide>
         ))}
